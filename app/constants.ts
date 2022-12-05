79ed7f10cbbb381af41a08522917759f43b0297d
Jonathan Rupp <jrupp@rightpoint.com>
2022-12-04 18:56:33 -0600

    more sensible suggestion for parallel download count

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
 // keep this up to date with Chrome releases to avoid 403s when using node-hls-downloader
 export const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36';
 // parallel downloads to suggest for node-hls-downloader
-export const parallelStreams = 100;
+export const parallelStreams = 5;
