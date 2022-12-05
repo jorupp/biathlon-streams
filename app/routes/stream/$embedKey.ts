2a8c86e456d72759fabfa155f1382cec6c02ca94
Jonathan Rupp <jrupp@rightpoint.com>
2022-12-04 18:41:37 -0600

    fix some issues with referrer headers and absolute URLs

--- app/routes/stream/$embedKey.ts
+++ app/routes/stream/$embedKey.ts
@@ -1,8 +1,9 @@
 import { LoaderFunction } from "remix";
+import { userAgent } from "~/constants";
 
 const headers = {
     'X-Requested-With': 'XMLHttpRequest',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+    'User-Agent': userAgent,
     'Accept-Language': 'en-US,en;q=0.9',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
@@ -13,7 +14,7 @@ const headers = {
 export async function loader({ params }: Parameters<LoaderFunction>[0]) {
     const { embedKey } = params;
     if (embedKey){
-        const referer = `https://ebu-emp.ebu.ch/embed/${embedKey}`
+        const referer = `https://ebu-emp.ebu.ch/embed/${embedKey}?shareLink=https%3A%2F%2Fwww.eurovisionsports.tv%2Fibu%23${embedKey}`
         const playUrl = `https://ebu-emp.ebu.ch/play/${embedKey}?_=${new Date().getTime()}`;
 
         const playData = await (await fetch(playUrl, {
@@ -25,7 +26,8 @@ export async function loader({ params }: Parameters<LoaderFunction>[0]) {
 
         if (playData && playData.url) {
                 
-            const masterData = await (await fetch(playData.url, {
+            const masterUrl = playData.url;
+            const masterData = await (await fetch(masterUrl, {
                 headers: {
                     ...headers,
                     Referer: referer,
@@ -42,7 +44,7 @@ export async function loader({ params }: Parameters<LoaderFunction>[0]) {
                     }
                     stream = {};
                     const metadata = Object.fromEntries(row.split(',').map(i => i.split('=')));
-                    console.log(JSON.stringify(metadata, null, 2));
+                    // console.log(JSON.stringify(metadata, null, 2));
                     if (metadata.BANDWIDTH) {
                         stream.bandwidth = parseInt(metadata.BANDWIDTH);
                     }
@@ -58,16 +60,32 @@ export async function loader({ params }: Parameters<LoaderFunction>[0]) {
 
             streams.sort((a,b) => (a.bandwidth ?? 0) > (b.bandwidth ?? 0) ? -1 : 1);
 
+            // console.log(streams);
+
             if (streams.length > 0) {
-                
-                const streamData = await (await fetch(streams[0].url!, {
+                const streamUrl = new URL(streams[1].url!, playData.url).href;
+                const streamData = await (await fetch(streamUrl, {
                     headers: {
                         ...headers,
                         Referer: referer,
                     }
                 })).text();
+
+                // rewrite the URLs to be absolute
+                const prefix = new URL('.', streamUrl).href;
+                // console.log({ masterUrl, streamUrl, prefix });
+                const outputStreamData = [];
+                for(const streamLine of streamData.split('\n')) {
+                    if (streamLine.startsWith('#')) {
+                        outputStreamData.push(streamLine);
+                    }
+                    else {
+                        outputStreamData.push(prefix + streamLine);
+                    }
+
+                }
                 
-                return new Response(streamData, {
+                return new Response(outputStreamData.join('\n'), {
                     status: 200,
                     headers: {
                         'Content-Type': "application/vnd.apple.mpegurl"
